9d219d54ccd75113dd7243b41eb7921b71823f37
Pawel3-linux <pawelek.rz@o2.pl>
2016-02-01 20:45:42 +0100

    Update accounts.system.hpp

--- accounts.system.hpp
+++ accounts.system.hpp
@@ -1,6 +1,6 @@
 #define AM_DEACCESS		0
 #define AM_STDUSER		1
-#define AM_MODERATOR	2
+#define AM_MODERATOR		2
 #define AM_ADMIN		3
 #define AM_ROOT			4
 class Account
@@ -15,5 +15,19 @@ class Account
 		{
 			access_mode = AM_DEACCESS;
 		}
-		
+		bool login(void)
+		{
+			char pswtab[30][100];
+			int acs_md[30];
+			char usrtab[30][100];
+			FILE * acfile = fopen("accounts.acc", "a+");
+			
+			cout << "Give me password: ";
+			char pswinput[100] = "";
+			cin >> pswinput;
+			system("clear");
+			cout << "Give username: ";
+			char username[100];
+			cin >> usrname;
+		}
 };
